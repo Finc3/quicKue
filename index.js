ab424d3a7716893918185675b657c7d6d8b905f2
Daniel Waardal <waawal@boom.ws>
2016-10-14 16:47:37 +0200

    fix: or -> ||

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var kue = require('kue');
 var basicAuth = require('basic-auth-connect');
 var queue = require('./queue');
-kue.app.use(basicAuth(process.env.USER_NAME or 'admin', process.env.PASSWORD));
-kue.app.set('title', process.env.TITLE or 'quicKue');
+kue.app.use(basicAuth(process.env.USER_NAME || 'admin', process.env.PASSWORD));
+kue.app.set('title', process.env.TITLE || 'quicKue');
 
-kue.app.listen(process.env.PORT or 3000);
+kue.app.listen(process.env.PORT || 3000);
--- queue.js
+++ queue.js
@@ -1,4 +1,4 @@
 var kue = require('kue');
 module.exports = kue.createQueue({
-  redis: process.env.REDIS_URL or 'redis://127.0.0.1:6379'
+  redis: process.env.REDIS_URL || 'redis://127.0.0.1:6379'
 });
