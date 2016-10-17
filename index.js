87be95e5b4d72bc8cdb4f2a522f5284686721e3c
Daniel Waardal <waawal@boom.ws>
2016-10-17 13:45:47 +0200

    createServer has been removed from express

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var kue = require('kue');
 var basicAuth = require('basic-auth-connect');
 var queue = require('./queue');
 var express = require('express');
-var app = express.createServer();
+var app = express();
 app.use(basicAuth(process.env.USER_NAME || 'admin', process.env.PASSWORD));
 kue.app.set('title', process.env.TITLE || 'quicKue');
 app.use(kue.app);
