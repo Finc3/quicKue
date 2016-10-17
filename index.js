e06692bbba615a041a39c7c9e6bfa43db44dd7fb
Daniel Waardal <waawal@boom.ws>
2016-10-17 13:17:47 +0200

    mounting kue app to express instance

--- index.js
+++ index.js
@@ -2,7 +2,11 @@
 var kue = require('kue');
 var basicAuth = require('basic-auth-connect');
 var queue = require('./queue');
-kue.app.use(basicAuth(process.env.USER_NAME || 'admin', process.env.PASSWORD));
+var express = require('express');
+var app = express.createServer();
+app.use(basicAuth(process.env.USER_NAME || 'admin', process.env.PASSWORD));
 kue.app.set('title', process.env.TITLE || 'quicKue');
+app.use(kue.app);
+app.listen(process.env.PORT || 3000);
 
-kue.app.listen(process.env.PORT || 3000);
+module.exports = app;
